45d1c44adb55466f5067e6555e41ec05aeb55482
Bingxiong Yi <yibingxiong@126.com>
2019-04-23 23:58:02 +0800

    404页面引导点星星

--- src/fe/pages/404/index.js
+++ src/fe/pages/404/index.js
@@ -3,7 +3,10 @@ import './index.scss';
 class Index extends React.Component {
     render() {
         return (
-            <div>404 not found</div>
+            <div className="container">
+                <div>404 not found</div>
+                <div>请到<a href="https://github.com/yibingxiong/web-test">web-test</a>点个小星星吧</div>
+            </div>
         )
     }
 }
