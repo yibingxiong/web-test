430741678559963e859c84d204123af2e5ff68fe
Bingxiong Yi <yibingxiong@126.com>
2019-04-24 14:00:35 +0800

    关于code-split的测试

---
+++ code-split/2/test2.js
@@ -0,0 +1,2 @@
+import './b.js'
+console.log('1')
---
+++ code-split/3/0.app.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./b.js":
+/*!**************!*\
+  !*** ./b.js ***!
+  \**************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\nconsole.log('b');\n\n//# sourceURL=webpack:///./b.js?");
+
+/***/ })
+
+}]);
